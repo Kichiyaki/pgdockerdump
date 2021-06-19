18b8285807646cc9e121efcb3ac1d4f9cd35a25a
Kichiyaki <xyztojajestem@gmail.com>
2021-06-19 09:48:47 +0200

    add one more function: deleteOldBackups

--- backup.sh
+++ backup.sh
@@ -6,5 +6,31 @@ downloadBackup() {
   containerName=$1
   user=$2
   dbName=$3
-  ssh -o ConnectTimeout=10 "$connectionString" "docker exec $containerName pg_dump -F c -U $user -d $dbName" >"$dbName-$(date +%s).dump"
+  if [ -z "$containerName" ] || [ -z "$user" ] || [ -z "$dbName" ]; then
+    echo "downloadBackup error: not enough arguments passed to this function"
+    return
+  fi
+  filename="$dbName-$(date +%s).dump"
+  ssh -o ConnectTimeout=10 "$connectionString" "docker exec $containerName pg_dump -F c -U $user -d $dbName" >"$filename"
+  zip "$filename.zip" "$filename"
+  rm -rf "$filename"
+}
+
+deleteOldBackups() {
+  dbName=$1
+  sub=$2
+  if [ -z "$dbName" ] || [ -z "$sub" ]; then
+    echo "deleteOldBackups error: not enough arguments passed to this function"
+    return
+  fi
+  files=($(ls *.dump.zip))
+  timestamp="$(date +%s)"
+  difference="$((timestamp - sub))"
+  for f in "${files[@]}"; do
+    IFS='-' read -ra parts <<<"${f/.dump.zip/}"
+    if [ "${#parts[@]}" != "2" ] || [ "${parts[0]}" != "$dbName" ] || [[ ${parts[1]} -ge $difference ]]; then
+      continue
+    fi
+    rm -rf "$f"
+  done
 }
